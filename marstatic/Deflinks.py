5559b81d98d64b74c809b882f414d9eafa24481f
mentalblood <neceporenkostepan@gmail.com>
2024-08-19 21:44:20 +0300

    html template, coloring, header

--- marstatic/Deflinks.py
+++ marstatic/Deflinks.py
@@ -1,5 +1,7 @@
 import colorsys
+import functools
 import re
+from dataclasses import dataclass
 
 from markdown import Markdown
 from markdown.extensions import Extension
@@ -12,20 +14,37 @@ class Deflinks(Extension):
         md.preprocessors.register(DeflinksPreprocessor(), "deflinks", 100)
 
 
-class DeflinksPreprocessor(Preprocessor):
-    deflink_regex = re.compile(r"\*\*(?P<link>(?:\w|\.|\/)+?)\*\*:*")
+@dataclass(frozen=True, kw_only=False)
+class Replacer:
+    deflink_regex = re.compile(r"\*\*([^А-Яа-я]+?)\*\*:*")
 
-    @staticmethod
-    def replace(m: re.Match):
+    lines: list[str]
+
+    def color(self, target: int, overall: int):
+        return "#%02x%02x%02x" % tuple(int(255 * i) for i in colorsys.hsv_to_rgb(target * 1 / overall, 0.34, 0.78))
+
+    @functools.cached_property
+    def defs(self):
+        return set(m.group(1) for m in re.finditer(self.deflink_regex, "\n".join(self.lines)))
+
+    @functools.cached_property
+    def defs_colors(self):
+        return {d: self.color(i, len(self.defs)) for i, d in enumerate(sorted(self.defs))}
+
+    def replace(self, m: re.Match):
         g = m.group(1)
         if m.group(0).endswith(":"):
-            return f'**{g}**<a name="{g}"></a>:'
+            return f'<span class="def" style="background-color:{self.defs_colors[g]}">&nbsp;**{g}**&nbsp;</span><a name="{g}"></a>:'
         else:
-            return f"**[{g}](#{g})**"
+            return (
+                f'<span class="link" style="background-color:{self.defs_colors[g]}">&nbsp;**[{g}](#{g})**&nbsp;</span>'
+            )
 
-    @staticmethod
-    def color(target: int, overall: int):
-        return "#%02x%02x%02x" % colorsys.hsv_to_rgb(target * 1 / overall, 0.5, 0.5)
+    @property
+    def result(self):
+        return [re.sub(self.deflink_regex, self.replace, l) for l in self.lines]
 
+
+class DeflinksPreprocessor(Preprocessor):
     def run(self, lines: list[str]):
-        return [re.sub(self.deflink_regex, self.replace, l) for l in lines]
+        return Replacer(lines).result
--- marstatic/__main__.py
+++ marstatic/__main__.py
@@ -2,15 +2,23 @@ import argparse
 import pathlib
 
 import markdown
+import ruiner
 
 from .Deflinks import Deflinks
 
 parser = argparse.ArgumentParser(prog="marstatic", description="Convert markdown to HTML")
 parser.add_argument("-i", "--input", type=pathlib.Path, required=True)
 parser.add_argument("-o", "--output", type=pathlib.Path, required=True)
+parser.add_argument("-H", "--header", type=str, required=True)
 
 args = parser.parse_args()
 
 args.output.write_text(
-    markdown.markdown(args.input.read_text(encoding="utf8"), extensions=[Deflinks()]), encoding="utf8"
+    ruiner.Template((pathlib.Path(__file__).parent / "template.html").read_text()).rendered(
+        {
+            "header": args.header,
+            "body": markdown.markdown(args.input.read_text(encoding="utf8"), extensions=[Deflinks()]),
+        }
+    ),
+    encoding="utf8",
 )
