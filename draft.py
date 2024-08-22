9910be3133313bd9f840202d34b54dee334c22ce
Нечепоренко Степан <necheporenko_s_iu@goznak.ru>
2024-08-22 13:51:38 +0300

    working draft for parser

--- draft.py
+++ draft.py
@@ -1,29 +1,34 @@
 import pyparsing as pp
 
-fundamental_root = pp.Combine(pp.Char(pp.alphas.upper()) * ...)("fundamental_root")
-root = pp.Combine(pp.Char(pp.alphas.lower()) * ...)("root")
-number = pp.Combine(pp.Char(pp.nums) * ...)("number")
-fundamental_atom = (fundamental_root + number)("fundamental_atom")
-atom = (root + number)("atom")
+fundamental_root = pp.Combine(pp.Char(pp.alphas.upper())[1, ...]).add_parse_action(lambda x: ("root", x[0]))
+root = pp.Combine(pp.Char(pp.alphas.lower())[1, ...]).add_parse_action(lambda x: ("root", x[0]))
+number = pp.Combine(pp.Char(pp.nums)[1, ...]).add_parse_action(lambda x: ("number", x[0]))
+fundamental_atom = (fundamental_root + pp.Opt(number)).add_parse_action(
+    lambda x: ("fundamental_atom", dict(x.as_list()))
+)
+atom = (root + pp.Opt(number)).add_parse_action(lambda x: ("atom", dict(x.as_list())))
 
-clarification_delimiter = pp.Char(".")
-clarification = (atom | fundamental_atom) + pp.Opt(
-    clarification_delimiter + pp.DelimitedList(atom | number, clarification_delimiter)
-)("clarification")
+clarification_delimiter = pp.Char(".").suppress()
+clarification = (
+    (atom | fundamental_atom) + clarification_delimiter + pp.DelimitedList(atom | number, clarification_delimiter)
+).set_parse_action(lambda x: ("clarification", x.as_list()))
 
-version_delimiter = pp.Char("-")
-version = (clarification | atom | fundamental_atom) + pp.Opt(
-    version_delimiter + pp.DelimitedList(atom | number, version_delimiter)
-)("version")
+version_delimiter = pp.Char("-").suppress()
+version = (
+    (clarification | atom | fundamental_atom) + version_delimiter + pp.DelimitedList(atom | number, version_delimiter)
+).set_parse_action(lambda x: ("version", x.as_list()))
 
-answer_delimiter = pp.Char("/")
+answer_delimiter = pp.Char("/").suppress()
 answer_element = version | clarification | atom | fundamental_atom
-answer = pp.DelimitedList(answer_element)("answer")
+answer = pp.DelimitedList(answer_element, answer_delimiter).set_parse_action(lambda x: ("answer", x.as_list()))
 
-thesis = (answer | version | clarification | fundamental_atom)("thesis")
+thesis = (answer | version | clarification | fundamental_atom).set_parse_action(lambda x: ("thesis", x.as_list()))
 
 
 def test_basic():
-    # print(atom.parse_string("abc1234").asDict())
-    print(clarification.parse_string("A2.b").asDict())
-    # print(thesis.parse_string("A2.b-c/D").asDict())
+    atom.parse_string("abc1234").pprint()
+    fundamental_atom.parse_string("ABC1234").pprint()
+    clarification.parse_string("A2.b").pprint()
+    clarification.parse_string("A2.b.c").pprint()
+    version.parse_string("A2.b-c").pprint()
+    answer.parse_string("A2.b-c/D").pprint()
