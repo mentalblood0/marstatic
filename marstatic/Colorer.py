8158ae570a692aab9c0330805f020b9b3eedc604
Нечепоренко Степан <necheporenko_s_iu@goznak.ru>
2024-08-28 17:41:58 +0300

    radical coloring simplification in favor of determinacy; make saturation and value also variable

--- marstatic/Colorer.py
+++ marstatic/Colorer.py
@@ -1,7 +1,7 @@
 import colorsys
 import dataclasses
-import enum
 import functools
+import hashlib
 import itertools
 import re
 import typing
@@ -13,110 +13,29 @@ tsid_parser = TsidParser()
 
 @dataclasses.dataclass(frozen=True, kw_only=False)
 class Color:
-    shift: float
-    saturation: float = 0.4
+    salt = b""
+    s_limits = (0, 1)
+    v_limits = (0, 1)
 
-    @property
-    def h(self):
-        return self.shift
-
-    @property
-    def s(self):
-        return self.saturation
-
-    @property
-    def v(self):
-        return 0.78
+    h: float
+    s: float
+    v: float
 
     def saturated(self, c: float):
-        return dataclasses.replace(self, saturation=self.saturation * c)
+        return dataclasses.replace(self, s=self.s * c)
 
     @property
     def css(self):
         return "#%02x%02x%02x" % tuple(int(255 * i) for i in colorsys.hsv_to_rgb(self.h, self.s, self.v))
 
     @classmethod
-    @functools.cache
-    def from_number(cls, number: int):
-        result = 0
-        d = 0.5
-        for b in bin(number + 1).split("b")[1]:
-            result += (1 if b == "1" else -1) * d
-            d /= 2
-        return cls(result)
-
-
-@functools.cache
-def fundamental_roots(o: T | T.Ans | T.V | T.C | T.r | T.A | T.N):
-    if isinstance(o, T):
-        return fundamental_roots(o.value)
-    if isinstance(o, T.Ans | T.V | T.C):
-        return {r for pattern in o.value for r in fundamental_roots(pattern)}
-    if isinstance(o, T.A):
-        return {o.root}
-    return set()
-
-
-def versions(o: T | T.Ans | T.V | T.C | T.r | T.A | T.N):
-    if isinstance(o, T):
-        return versions(o.value)
-    if isinstance(o, T.Ans | T.C):
-        return {r for pattern in o.value for r in versions(pattern)}
-    if isinstance(o, T.V):
-        return {o}
-    return set()
-
-
-@dataclasses.dataclass(frozen=True, kw_only=False)
-class Tsid:
-    value: str
-
-    @functools.cached_property
-    def parsed(self):
-        return tsid_parser.parse(self.value)
-
-    @functools.cached_property
-    def fundamental_roots(self):
-        return fundamental_roots(self.parsed)
-
-    @functools.cached_property
-    def versions(self):
-        return versions(self.parsed)
-
-    def __lt__(self, o: object):
-        if isinstance(o, Tsid):
-            return self.value < o.value
-        raise ValueError(f"{self} < {o}")
-
-
-@dataclasses.dataclass(frozen=True, kw_only=False)
-class Colorspace:
-    members: set[T.R]
-
-    @functools.cached_property
-    def number_by_value(self):
-        return {r.value: i for i, r in enumerate(sorted(self.members, key=lambda r: r.value))}
-
-    def color(self, o: T.R | T.A):
-        if isinstance(o, T.R):
-            return Color.from_number(self.number_by_value[o.value])
-        if isinstance(o, T.A):
-            return Color.from_number(self.number_by_value[o.root.value])
-
-
-@dataclasses.dataclass(frozen=True, kw_only=False)
-class VersionColorspace:
-    First = T.V.First | T.V | None
-
-    first: First
-    members: set[T.V.Other]
-
-    @functools.cached_property
-    def number_by_value(self):
-        return {r.value: i for i, r in enumerate(sorted(self.members, key=lambda r: r.value))}
-
-    def color(self, o: T.V):
-        return Color.from_number(self.number_by_value[o.value[-1].value])
+    def from_value(cls, value: str):
+        digest = hashlib.blake2b(value.encode(), digest_size=6, salt=cls.salt).digest()
+        return cls(
+            int.from_bytes(digest[:2]) / 2**16,
+            cls.s_limits[0] + int.from_bytes(digest[2:4]) / 2**16 * (cls.s_limits[1] - cls.s_limits[0]),
+            cls.v_limits[0] + int.from_bytes(digest[4:]) / 2**16 * (cls.v_limits[1] - cls.v_limits[0]),
+        )
 
 
 @dataclasses.dataclass(frozen=True, kw_only=False)
@@ -162,15 +81,10 @@ class Colored:
 
 @dataclasses.dataclass(frozen=True, kw_only=False)
 class Colorer:
-    class VersionsColorspaceMode(enum.Enum):
-        MANY = "many"
-        ONE = "one"
-        SHARED = "shared"
 
     default_tsid_heuristic = re.compile(r"\*\*([^А-Яа-я]+?)\*\*:?")
 
     text: str
-    versions_colorspace_mode: VersionsColorspaceMode
     tsid_heuristic: re.Pattern[str] = default_tsid_heuristic
 
     @functools.cached_property
@@ -180,36 +94,20 @@ class Colorer:
     @functools.cached_property
     def tsids(self):
         return {
-            Tsid(m.group(1))
-            for m in itertools.chain.from_iterable(re.finditer(self.tsid_heuristic, l) for l in self.lines)
+            m.group(1): f"i{i}"
+            for i, m in enumerate(
+                itertools.chain.from_iterable(re.finditer(self.tsid_heuristic, l) for l in self.lines)
+            )
         }
 
-    @functools.cached_property
-    def fundamental_roots(self):
-        return {r for t in self.tsids for r in t.fundamental_roots}
-
-    @functools.cached_property
-    def versions(self):
-        return {r for t in self.tsids for r in t.versions}
-
-    @typing.overload
-    def colorspace(self, first: None = None) -> Colorspace: ...
-    @typing.overload
-    def colorspace(self, first: VersionColorspace.First) -> VersionColorspace: ...
-    @functools.cache
-    def colorspace(self, first: VersionColorspace.First | None = None):
-        if first is None:
-            return Colorspace(self.fundamental_roots)
-        elif isinstance(first, T.Ans | T.C | T.A):
-            return VersionColorspace(
-                None,
-                {
-                    o
-                    for v in self.versions
-                    if (v.first == first) or self.versions_colorspace_mode == Colorer.VersionsColorspaceMode.ONE
-                    for o in v.other
-                },
-            )
+    def color(self, o: T.A | T.R | T.V):
+        if isinstance(o, T.R):
+            value = o.value
+        elif isinstance(o, T.A):
+            value = o.root.value
+        elif isinstance(o, T.V):
+            value = str(o.value[-1].value)
+        return Color.from_value(value)
 
     def flatten(self, l: list):
         result = []
@@ -220,24 +118,16 @@ class Colorer:
                 result += self.flatten(e)
         return result
 
-    @functools.cached_property
-    def number_by_tsid(self):
-        return {r.value: i for i, r in enumerate(sorted(self.tsids))}
-
     def replace(self, m: re.Match):
         e = m.group(1)
-        return self.colored(e).html(f"i{self.number_by_tsid[e]}", link=not m.group(0).endswith(":")) + (
+        return self.colored(e).html(self.tsids[e], link=not m.group(0).endswith(":")) + (
             ":" if m.group(0)[-1] == ":" else ""
         )
 
     @functools.cached_property
     def css(self):
         return sorted(
-            [
-                {"class": f"i{self.number_by_tsid[t.value]}", "background": self.colored(t.value).css()}
-                for t in self.tsids
-            ],
-            key=lambda c: c["class"],
+            [{"class": i, "background": self.colored(t).css()} for t, i in self.tsids.items()], key=lambda c: c["class"]
         )
 
     @typing.overload
@@ -257,28 +147,21 @@ class Colorer:
         if isinstance(o, T):
             return self.colored(o.value)
 
-        result = []
         if isinstance(o, T.R | T.A):
-            result = [(o.loc, self.colorspace().color(o))]
+            return [(o.loc, self.color(o))]
 
         elif isinstance(o, T.V):
             result = self.colored(o.first)
             for i in range(len(o.other)):
                 oo = o.other[i]
-                vcs_root = T.V(o.first, *o.other[:i]) if i > 1 else o.first
-                vcs_query = T.V(o.first, *o.other[: i + 1])
-                assert oo.loc is not None
                 if oo.loc is None:
                     raise ValueError(f"{oo} loc is {oo.loc}")
-                result.append((oo.loc, self.colorspace(vcs_root).color(vcs_query)))
+                result.append((oo.loc, self.color(T.V(o.first, *o.other[: i + 1]))))
+            return result
 
         elif isinstance(o, T.C):
             first = self.colored(o.first)
-            result = first + [(c.loc, first[-1][1].saturated(0.57 ** (i + 1))) for i, c in enumerate(o.other)]
+            return first + [(c.loc, first[-1][1].saturated(0.57 ** (i + 1))) for i, c in enumerate(o.other)]
 
         elif isinstance(o, T.Ans):
-            result = [self.colored(o.first)]
-            for a in o.other:
-                result += self.colored(a)
-
-        return self.flatten(result)
+            return self.flatten([self.colored(o.first)] + [self.colored(a) for a in o.other])
--- marstatic/__main__.py
+++ marstatic/__main__.py
@@ -4,29 +4,31 @@ import pathlib
 import markdown
 import ruiner
 
-from .Colorer import Colorer
+from .Colorer import Color, Colorer
 
 parser = argparse.ArgumentParser(prog="marstatic", description="Convert markdown to HTML")
 
-parser.add_argument("-i", "--input", type=pathlib.Path, required=True)
-parser.add_argument("-o", "--output", type=pathlib.Path, required=True)
-parser.add_argument(
-    "-vc",
-    "--versions_colorspace",
-    type=Colorer.VersionsColorspaceMode,
-    choices=[c for c in Colorer.VersionsColorspaceMode],
-    required=True,
-)
-parser.add_argument("-H", "--header", type=str, required=True)
+parser.add_argument("input", type=pathlib.Path)
+parser.add_argument("-o", "--output", type=pathlib.Path, required=False)
+parser.add_argument("-t", "--title", type=str, required=False, default="notes")
+parser.add_argument("-s", "--salt", type=str, required=False, default="al")
+parser.add_argument("-si", "--saturation_min", type=float, required=False, default=0.25)
+parser.add_argument("-sa", "--saturation_max", type=float, required=False, default=0.7)
+parser.add_argument("-vi", "--value_min", type=float, required=False, default=0.65)
+parser.add_argument("-va", "--value_max", type=float, required=False, default=0.95)
 
 args = parser.parse_args()
 
+Color.salt = args.salt.encode()
+Color.s_limits = (args.saturation_min, args.saturation_max)
+Color.v_limits = (args.value_min, args.value_max)
+
 text = args.input.read_text(encoding="utf8")
-c = Colorer(text, args.versions_colorspace)
+c = Colorer(text)
 body = markdown.markdown(c.colored())
 
 templates = {p.stem: ruiner.Template(p.read_text()) for p in (pathlib.Path(__file__).parent / "templates").iterdir()}
-parameters = {"header": str(args.header), "body": body, "Color": c.css}
+parameters = {"title": str(args.title), "body": body, "Color": c.css}
 
 result = templates["Page"].rendered(parameters, templates)
-args.output.write_text(result, encoding="utf8")
+(args.output or args.input.with_suffix(".html")).write_text(result, encoding="utf8")
